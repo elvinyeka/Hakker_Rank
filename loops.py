dd22f802b106444c8eecd2e3910d97351944be0f
elvin <elvinyeka@gmail.com>
2020-09-10 19:30:47 +0400

    hakker_rank

---
+++ loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(10):
+    i+=1
+    result = 2*i
+    print(f"2 x {i} = {result}")
