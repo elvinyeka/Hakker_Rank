e0730e33d1076251d6e84f7ea50d2fda28febaf6
elvin <elvinyeka@gmail.com>
2020-09-10 20:47:45 +0400

    operathors

---
+++ arifmethicoperathors.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    x=a+b
+    y = a-b
+    z = a*b
+    print(x,y,z,sep="\n")
+
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
