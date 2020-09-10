e3b9bd87b7be2297655345493022cc1b637c86b1
elvin <elvinyeka@gmail.com>
2020-09-10 21:14:52 +0400

    loops-python

---
+++ loops-python.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        kvadrat = i ** 2
+        print(kvadrat)
+
+
+#https://www.hackerrank.com/challenges/python-loops/problem
