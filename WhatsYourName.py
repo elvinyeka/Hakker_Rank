1db4031927490e28d1ae62ccf398811b83540fc4
elvin <elvinyeka@gmail.com>
2020-09-13 10:19:03 +0400

    yeni tasklar

---
+++ WhatsYourName.py
@@ -0,0 +1,14 @@
+def print_full_name(a, b):
+    a = first_name
+    b = last_name
+
+    print(f"Hello {a} {b}! You just delved into python.")
+
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+
+# https://www.hackerrank.com/challenges/whats-your-name/problem
