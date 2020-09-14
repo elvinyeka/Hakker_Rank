779ef7deeef9ea7da3ea265de60d8e1a2b6c7ce9
elvin <elvinyeka@gmail.com>
2020-09-14 15:31:18 +0400

    yeni task

---
+++ listComprehensions.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    result = []
+    for i in range(x + 1):
+        for j in range(y + 1):
+            for k in range(z + 1):
+                if i + j + k != n:
+                    result.append([i, j, k])
+
+    print(result)
+
+
+#https://www.hackerrank.com/challenges/list-comprehensions/problem
